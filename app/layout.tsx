3d107eb986d0319639e8411f53ffdb5baa95cc24
CandraSetiawan <candra12305@gmail.com>
2023-08-20 13:23:43 +0700

    upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} mx-auto px-8 md:px-72 bg-zinc-900`}>{children}</body>
     </html>
   )
 }
